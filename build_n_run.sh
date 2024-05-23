bad6ea9cc78ed831b6163aec5cb1e224ec80d49f
João Pardal <78451368+Joao8bit@users.noreply.github.com>
2024-05-24 00:17:47 +0200

    Shared volume conflicts with git cloning, this aliviates the issue for now

--- build_n_run.sh
+++ build_n_run.sh
@@ -1,4 +1,4 @@
 docker build -t ros_humble_venv .
-docker run -it --name ros_humble --net=host --privileged -e DISPLAY=$DISPLAY\
-  -v ~/ros2_docker/:/root/ros_ws:rw \
+docker run -it --name ros_humble --net=host\
+  --privileged -e DISPLAY=$DISPLAY\
   --pid=host ros_humble_venv
