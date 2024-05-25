d1e0c0604a3ebc79d08fd36a9cd4fbc1fbea8065
João Pardal <78451368+Joao8bit@users.noreply.github.com>
2024-05-25 20:33:37 +0200

    Update native.sh

--- native.sh
+++ native.sh
@@ -1,27 +1,27 @@
 export ROS_DISTRO=humble
 # Dependencies installation
- apt update && apt install --no-install-recommends -y \
+apt update && apt install --no-install-recommends -y \
     ninja-build gettext cmake unzip curl build-essential xterm python3-venv \
     ros-$ROS_DISTRO-rmw-cyclonedds-cpp ros-$ROS_DISTRO-rviz2 iputils-ping \
     ros-$ROS_DISTRO-rtabmap-ros ros-$ROS_DISTRO-navigation2 ros-$ROS_DISTRO-nav2-bringup ros-$ROS_DISTRO-leo-description
 
 # Preparing ROS2 directly at boot of the docker
- echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 
 # Getting all the source code into the ROS workspace
- mkdir -p ~/ros_ws/src
- cd ~/ros_ws/src/
- git clone https://github.com/snt-spacer/leo_simulator-ros2.git
- git clone https://github.com/snt-spacer/leo_common-ros2.git
- git clone https://github.com/snt-spacer/rtabmap_livox.git
+mkdir -p ~/ros_ws/src
+cd ~/ros_ws/src/
+git clone https://github.com/snt-spacer/leo_simulator-ros2.git
+git clone https://github.com/snt-spacer/leo_common-ros2.git
+git clone https://github.com/snt-spacer/rtabmap_livox.git
 
 # Moving to ws dir to install dependencies
 cd ~/ros_ws/
 # remove the rosdep sources list if it exists already
- rm /etc/ros/rosdep/sources.list.d/20-default.list
- rosdep init
- rosdep update
- rosdep install --from-paths src -y --ignore-src
+rm /etc/ros/rosdep/sources.list.d/20-default.list
+rosdep init
+rosdep update
+rosdep install --from-paths src -y --ignore-src
 
 # Everything is complete, don't forget to build the workspace!
 # This command is greyed because we need to source ROS2 at the same time as we run it,
@@ -29,4 +29,4 @@ cd ~/ros_ws/
 # RUN colcon build
 
 # Proposed solution: (https://stackoverflow.com/questions/72727733/how-to-use-colcon-build-in-a-dockerfile)
-# RUN /bin/bash -c "source /opt/ros/$ROS_DISTRO/setup.bash; colcon build"
+/bin/bash -c "source /opt/ros/$ROS_DISTRO/setup.bash; colcon build"
