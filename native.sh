7fc63a81869c6d2e470f081c86597f3e275d12c6
João Pardal <78451368+Joao8bit@users.noreply.github.com>
2025-04-22 10:39:47 +0200

    Update native.sh

--- native.sh
+++ native.sh
@@ -6,7 +6,7 @@ export ROS_DISTRO=jazzy
 sudo apt install --no-install-recommends -y \
     ninja-build git colcon python3-rosdep gettext cmake unzip curl build-essential xterm python3-venv \
     ros-$ROS_DISTRO-rmw-cyclonedds-cpp ros-$ROS_DISTRO-rviz2 iputils-ping \
-    ros-$ROS_DISTRO-rtabmap-ros ros-$ROS_DISTRO-navigation2 ros-$ROS_DISTRO-nav2-bringup ros-$ROS_DISTRO-leo-description ros-$ROS_DISTRO-ros-gz ros-$ROS_DISTRO-rtabmap-odom ros-$ROS_DISTRO-rtabmap-slam
+    ros-$ROS_DISTRO-rtabmap-ros ros-$ROS_DISTRO-navigation2 ros-$ROS_DISTRO-nav2-bringup ros-$ROS_DISTRO-leo-description ros-$ROS_DISTRO-ros-gz ros-$ROS_DISTRO-rtabmap-odom ros-$ROS_DISTRO-rtabmap-slam \
 ros-$ROS_DISTRO-rtabmap-viz
 
 # Preparing ROS2 directly at boot of the docker
