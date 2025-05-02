c0c0aabf79fa9068de03893488d39d9fb93ba102
João Pardal <78451368+Joao8bit@users.noreply.github.com>
2025-05-02 14:28:23 +0200

    Update build_n_run.sh

--- build_n_run.sh
+++ build_n_run.sh
@@ -1,5 +1,5 @@
-docker build -t ros_humble_venv .
-docker run -it --name ros_humble --net=host\
+docker build -t ros_jazzy_venv .
+docker run -it --name ros_jazzy --net=host\
   --volume /dev/input:/dev/input\
   --privileged -e DISPLAY=$DISPLAY\
-  --pid=host ros_humble_venv
+  --pid=host ros_jazzy_venv
