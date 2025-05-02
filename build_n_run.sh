4757ea4aa7ecf38b99b1a94b559ed287fc7f67dc
João Pardal <78451368+Joao8bit@users.noreply.github.com>
2025-05-02 11:48:22 +0200

    Update build_n_run.sh

--- build_n_run.sh
+++ build_n_run.sh
@@ -1,7 +1,7 @@
 mkdir -p $HOME/ros2_ws/src
 git clone -b humble https://github.com/snt-spacer/leo_simulator-ros2.git $HOME/ros2_ws/src/leo_simulator-ros2
 git clone -b humble https://github.com/snt-spacer/leo_common-ros2.git $HOME/ros2_ws/src/leo_common-ros2
-git clone https://github.com/snt-spacer/rtabmap_livox.git ~/ros_ws/src/
+git clone https://github.com/snt-spacer/rtabmap_livox.git $HOME/ros2_ws/src/
 
 sudo docker build -t ros_humble_venv .
 sudo docker run -it --name ros_humble --net host\
