1259bdb7ad7939a0afeb58ff823d733d827a14ab
João Pardal <78451368+Joao8bit@users.noreply.github.com>
2025-05-02 11:38:38 +0200

    Update build_n_run.sh

--- build_n_run.sh
+++ build_n_run.sh
@@ -1,5 +1,6 @@
-docker build -t ros_humble_venv .
-docker run -it --name ros_humble --net=host\
+sudo docker build -t ros_humble_venv .
+sudo docker run -it --name ros_humble --net host\
   --volume /dev/input:/dev/input\
+  -v $HOME/ros2_ws:/root/ros2_ws\
   --privileged -e DISPLAY=$DISPLAY\
   --pid=host ros_humble_venv
